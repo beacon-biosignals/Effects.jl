a8aeed41deaca7054ef6518b03d828cb3c58600e
Phillip Alday <palday@users.noreply.github.com>
2024-03-06 11:04:30 -0600

    use TestSetExtensions to show test progress (#64)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,26 @@
 using Aqua
 using Effects
 using Test
+using TestSetExtensions
 
-@testset "Aqua" begin
-    Aqua.test_all(Effects; ambiguities=false)
-end
+@testset ExtendedTestSet "Effects.jl" begin
+    @testset "Aqua" begin
+        Aqua.test_all(Effects; ambiguities=false)
+    end
 
-@testset "TypicalTerm" begin
-    include("typical.jl")
-end
+    @testset "TypicalTerm" begin
+        include("typical.jl")
+    end
 
-@testset "linear regression" begin
-    include("linear_regression.jl")
-end
+    @testset "linear regression" begin
+        include("linear_regression.jl")
+    end
 
-@testset "delta method" begin
-    include("delta_method.jl")
-end
+    @testset "delta method" begin
+        include("delta_method.jl")
+    end
 
-@testset "emmeans" begin
-    include("emmeans.jl")
+    @testset "emmeans" begin
+        include("emmeans.jl")
+    end
 end
