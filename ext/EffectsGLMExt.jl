0e67ea8739478768756a779ad4cd054b9a3e62e6
Phillip Alday <phillip.alday@beacon.bio>
2026-04-21 17:23:41 -0600

    Fix GLM AutoInvLink derivative handling and tighten compat

--- ext/EffectsGLMExt.jl
+++ ext/EffectsGLMExt.jl
@@ -2,13 +2,13 @@ module EffectsGLMExt
 
 using Effects
 
-using GLM: AbstractGLM, Link, Link01, inverselink
+using GLM: AbstractGLM, Link, Link01, linkinv, mueta
 using Effects.StatsModels: TableRegressionModel
 
 # TODO: upstream a Link(::TableRegressionModel{<:AbstractGLM})
 Effects._model_link(m::TableRegressionModel{<:AbstractGLM}, ::AutoInvLink) = Link(m.model)
 Effects._model_link(m::AbstractGLM, ::AutoInvLink) = Link(m)
-Effects._invlink_and_deriv(link::Link01, η) = inverselink(link, η)[1:2:3]  # (µ, 1 - µ, dμdη)
-Effects._invlink_and_deriv(link::Link, η) = inverselink(link, η)[1:2]  # (µ, dμdη, NaN)
+Effects._invlink_and_deriv(link::Link01, η) = (linkinv(link, η), mueta(link, η))
+Effects._invlink_and_deriv(link::Link, η) = (linkinv(link, η), mueta(link, η))
 
 end # module
